99f6ac8c7f3ea8eaee8a886185a362db3c515510
Gabriel Eduardo Acosta Nuñez <gacosta.com.do@outlook.com>
2019-11-14 02:13:06 -0400

    ..

---
+++ WeatherMap/WeatherMap/Models/Clima.cs
@@ -0,0 +1,30 @@
+﻿
+namespace WeatherMap.Models
+{
+   public class Clima
+    {
+        public string Titulo { get; set; }
+
+        public string Temperatura { get; set; }
+
+        public string Viento { get; set; }
+        public string Humedad { get; set; }
+
+        public string Visibilidad { get; set; }
+        public string Amanecer { get; set; }
+
+        public string Ocaso { get; set; }
+
+
+        public Clima()
+        {
+            this.Titulo = string.Empty;
+            this.Temperatura = "";
+            this.Viento = string.Empty;
+            this.Humedad= string.Empty;
+            this.Visibilidad= string.Empty;
+            this.Amanecer= string.Empty;
+            this.Ocaso= string.Empty;
+        }
+    }
+}
